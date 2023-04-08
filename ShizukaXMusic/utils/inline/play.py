4bc75634cc8a851bbe06731fb385cd9221509455
VAMSI REDDY <118047800+Python-Coder-xD@users.noreply.github.com>
2023-04-08 12:40:56 +0530

    Update play.py

--- ShizukaXMusic/utils/inline/play.py
+++ ShizukaXMusic/utils/inline/play.py
@@ -37,11 +37,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             InlineKeyboardButton(
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
-            ),
-            InlineKeyboardButton(
-                text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://github.com/its-star-boi/ShizukaXMusic"
-            ),
-        ],
+            ),  
         [
             InlineKeyboardButton(
                 text=_["PL_B_3"],
